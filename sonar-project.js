66bb24d563e806c7e3404a8c16c7c797ba9da1c1
Anildth <131136819+Anildth@users.noreply.github.com>
2024-05-23 15:12:14 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'https://sonarqube-notifcation-hub-devops.apps.cluster-xs46k.xs46k.sandbox961.opentlc.com/account/security/',
+  //serverUrl: 'https://sonarqube-notifcation-hub-devops.apps.cluster-xs46k.xs46k.sandbox961.opentlc.com/account/security/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsapp',
-	    'sonar.login': 'sqa_001ff1de5749d807047ab424508545333b1a077c',
+	    //'sonar.login': 'sqa_001ff1de5749d807047ab424508545333b1a077c',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
